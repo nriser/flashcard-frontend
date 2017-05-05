c5c03be720a824c6b8e9f6b68b5875265ab48e2d
nriser <riserika@gmail.com>
2017-05-05 12:05:01 -0400

    Hide/show on successful sign-in and sign-out
    Make .flashcard-container, .flashcard-container-header, and footer
    show when user successfully signs in, and hide when user successfully
    signs out.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,7 +5,7 @@ const store = require('../store.js')
 // $('.status-message').show()
 
 const signUpSuccess = (response) => { // argument can be (response) or something too. Just an argument name
-  // $('#sign-up').hide()
+  // $('#sign-up').toggleClass( "hide")
   // $('#sign-in').fadeIn()
   console.log(response)
   $('.status-message').text('You have successfully signed up! Please sign in.')
@@ -18,10 +18,22 @@ const signUpFailure = () => {
 const signInSuccess = (response) => { // argument can be (response) or something too. Just an argument name
   // because i know that i'll need that token again later, i'll store it somewhere
   console.log(response)
-  $('#change-password').fadeIn()
-  $('#sign-out').fadeIn()
-  $('#sign-in').hide()
-  $('#sign-up').hide()
+  // $('.header').hide()
+
+  // $('#myModal').modal('toggle');
+  $('#modal-signin').modal('hide')
+  // resource:  http://stackoverflow.com/questions/13183630/how-to-open-a-bootstrap-modal-window-using-jquery
+  // $('#myModal').modal('hide');
+
+  // $('body.modal-open').toggleClass('modal-open')
+  // $('div.modal-backdrop').toggleClass('modal-backdrop')
+  // $('div.modal-backdrop').toggleClass('fade')
+  // $('div.modal-backdrop').toggleClass('in')
+
+  $('.header').hide()
+  $('.flashcard-container').show()
+  $('.flashcard-container-header').show()
+  $('footer').show()
 
   store.user = response.user // response.user is the email id and token // stores whatever that was in that response
   $('.status-message').text('You have successfully signed in! Create a new game to play!')
@@ -41,11 +53,23 @@ const changePasswordFailure = () => {
 }
 
 const signOutSuccess = () => {
-  $('.status-message').text('You have successfully signed out!')
-  $('#change-password').hide()
-  $('#sign-out').hide()
-  $('#sign-in').fadeIn()
-  $('#sign-up').fadeIn()
+  // $('.status-message').text('You have successfully signed out!')
+
+  // $('.flashcard-container').hide()
+  // $('footer').hide()
+  // $('.header').show()
+  // $('.modal-signin').hide()
+
+  // $('body.modal-open').toggleClass('modal-open')
+  // $('div.modal-backdrop').toggleClass('modal-backdrop')
+  // $('div.modal-backdrop').toggleClass('fade')
+  // $('div.modal-backdrop').toggleClass('in')
+
+  $('.header').show()
+  $('.flashcard-container').hide()
+  $('.flashcard-container-header').hide()
+  $('footer').hide()
+
   store.user = null // only have one person signed in in a givne session, one browser
 }
 
