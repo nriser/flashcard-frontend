513dd04f98ab24933c9fbb05bfb026a733ced388
nriser <riserika@gmail.com>
2017-05-09 10:29:01 -0400

    Incorporate use of 3rd party text-to-speech API
    Add audio for each flashcard that speaks the Japanese pronunciation of
    each Japanese word.

---
+++ scripts/get-voice.sh
@@ -0,0 +1,2 @@
+curl --get --include 'https://voicerss-text-to-speech.p.mashape.com/?key=cb75676b142f4d9ea40f9082ad50731b&c=mp3&f=8khz_8bit_mono&hl=en-us&r=0&src=Hello%2C+world!' \
+  -H 'X-Mashape-Key: qPKjKSb9PSmshOR4GoCO2nlp2ru3p1v6nrjjsn2arMbLhfjgIM'
